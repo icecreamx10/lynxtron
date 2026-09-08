229a2e0a8eb483238d9bc8120ef8c19e2db4ade2
Wang Zhixuan <201550585+icecreamx10@users.noreply.github.com>
2026-09-08 16:58:23 +0800

    [BugFix][Runtime] Download missing runtimes from Lynxtron releases

--- src/packages/lynxtron/install.js
+++ src/packages/lynxtron/install.js
@@ -1,11 +1,5 @@
 import { getPostinstallRuntimeOptions } from './install-policy.js';
 import { ensureRuntime } from './runtime-manager.js';
-import { BASE_URL } from './utils/env-config.js';
 
 const options = getPostinstallRuntimeOptions();
-const { customUrl } = options;
-if (!customUrl && !BASE_URL) {
-  console.log('Lynxtron base URL is empty; skipping runtime download');
-} else {
-  await ensureRuntime(options);
-}
+await ensureRuntime(options);
